7b0df1155ff2f26a94f4e602ee604e401a066100
Abulfaz Mustafazade <74773667+abulfazmustafazade@users.noreply.github.com>
2026-08-13 00:24:34 +0400

    Integrate Firebase Analytics into firebase.js
    Added Firebase Analytics import and configuration.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, update, get, onValue, off, child, runTransaction } from 'firebase/database';
 
-// =====================================================================
-// 🔥 FIREBASE CONFIG — buraya öz config-inizi yazın
-// =====================================================================
+
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAb9XY6BLmmjX8Xw7YyFmoOJP55FiY45JU",
   authDomain: "quizdata11.firebaseapp.com",
@@ -18,7 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// =====================================================================
 
 export const FIREBASE_OK = firebaseConfig.apiKey !== "";
 
