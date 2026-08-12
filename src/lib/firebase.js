d67a7380cfe08bfe857f77f1ba03679899aa075f
Abulfaz Mustafazade <74773667+abulfazmustafazade@users.noreply.github.com>
2026-08-13 00:36:34 +0400

    Update Firebase API key placeholder

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const FIREBASE_OK = firebaseConfig.apiKey !== "AIzaSyAb9XY6BLmmjX8Xw7YyFmoOJP55FiY45JU";
+export const FIREBASE_OK = firebaseConfig.apiKey !== "YOUR_API_KEY_HERE";
 
 let db = null;
 
