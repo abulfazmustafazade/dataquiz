2e10772cb61c300a8d938ba65da6df0eb870cfe6
Abulfaz Mustafazade <74773667+abulfazmustafazade@users.noreply.github.com>
2026-08-13 00:34:54 +0400

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const FIREBASE_OK = firebaseConfig.apiKey !== "";
+export const FIREBASE_OK = firebaseConfig.apiKey !== "AIzaSyAb9XY6BLmmjX8Xw7YyFmoOJP55FiY45JU";
 
 let db = null;
 
